cf55b3327a75e99d5c94e6b1a8972a270298c8e4
Marie Chanaron <chanaronmarie@gmail.com>
2023-04-19 22:17:26 +0200

    display properly cars and categories

--- src/main/java/fr/mariech/voitures/servlet/ListCarServlet.java
+++ src/main/java/fr/mariech/voitures/servlet/ListCarServlet.java
@@ -15,12 +15,10 @@ public class ListCarServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         if (session.getAttribute("cars") == null) {
-            System.out.println("In list-car servlet");
             CarDao carDao = new CarDao();
             List<Car> cars = carDao.fetchCars();
             System.out.println(cars);
             session.setAttribute("cars", cars);
-            request.getRequestDispatcher("/WEB-INF/list-cars/").forward(request, response);
         }
     }
 
