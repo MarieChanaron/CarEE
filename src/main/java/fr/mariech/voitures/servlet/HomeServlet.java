15d9f9481c41f4fea68a92d8b306f8ce3a7a8074
Marie Chanaron <chanaronmarie@gmail.com>
2023-04-19 10:48:14 +0200

    initialize project

---
+++ src/main/java/fr/mariech/voitures/servlet/HomeServlet.java
@@ -0,0 +1,20 @@
+package fr.mariech.voitures.servlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "HomeServlet", value = "/")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/home/").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
